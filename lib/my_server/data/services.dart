dec2f27702d4bb14a3a0bdee0be04e0cd3ae2081
poseidon <61605433+poseidonKey@users.noreply.github.com>
2022-07-26 16:56:04 +0900

    대략 1차 성공

--- lib/my_server/data/services.dart
+++ lib/my_server/data/services.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:my_karaoke_firebase/models/song_model.dart';
 
@@ -5,18 +6,34 @@ class Services {
   static var client = http.Client();
 
   static Future<List<Song>?> fetchProducts() async {
-    // var response = await client.get(Uri.parse('https://makeup-api.herokuapp.com'
-    //     '/api/v1/products.json?brand=maybelline'));
+    // var response = await client
+    //     .get(Uri.parse('http://122.37.216.171:12345/my_karaoke_db/')); //외부
     var response = await client
-        .get(Uri.parse('http://122.37.216.171:12345/my_karaoke_db/')); //외부
-    // var response = await client.get(Uri.parse('http://192.168.219.107/my_karaoke_db/')); // 내부
+        .get(Uri.parse('http://192.168.219.107/my_karaoke_db/')); // 내부
 
     if (response.statusCode == 200) {
-      var jasonData = response.body;
-      //  return productFromJson(jasonData);
-      // print(jasonData);
+      var jsonData = response.body;
+      return songFromJson(jsonData);
+      // print(jsonData);
     } else {
-      return null;
+      return [];
     }
   }
+}
+
+List<Song> songFromJson(String str) => List<Song>.from(
+      json.decode(str).map(
+            (x) => fromJson(x),
+          ),
+    );
+Song fromJson(Map<String, dynamic> json) {
+  return Song(
+      json["id"] ?? "1",
+      json["songOwnerId"],
+      json["songName"],
+      json["songGYNumber"],
+      json["songTJNumber"],
+      json["songJanre"],
+      json["songUtubeAddress"],
+      json["songETC"]);
 }
--- lib/my_server/views/song_tile.dart
+++ lib/my_server/views/song_tile.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:my_karaoke_firebase/models/song_model.dart';
 
 class SongTile extends StatelessWidget {
@@ -12,22 +11,22 @@ class SongTile extends StatelessWidget {
       elevation: 2,
       child: Padding(
         padding: const EdgeInsets.all(8),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Stack(
-              children: [
-                Container(
-                  height: 75,
-                  width: 100,
-                  clipBehavior: Clip.antiAlias,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(4),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    height: 75,
+                    width: 100,
+                    clipBehavior: Clip.antiAlias,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    child: Text(song.songName),
                   ),
-                  child: Text(song.songName),
-                ),
-                Obx(
-                  () => CircleAvatar(
+                  CircleAvatar(
                     backgroundColor: Colors.white,
                     radius: 15,
                     child: IconButton(
@@ -38,46 +37,46 @@ class SongTile extends StatelessWidget {
                       iconSize: 18,
                     ),
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 8),
-            Text(
-              song.songJanre,
-              maxLines: 2,
-              style: const TextStyle(fontWeight: FontWeight.w400),
-              overflow: TextOverflow.ellipsis,
-            ),
-            const SizedBox(height: 8),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.green,
-                borderRadius: BorderRadius.circular(12),
-              ),
-              padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    song.id.toString(),
-                    style: const TextStyle(color: Colors.white),
-                  ),
-                  const Icon(
-                    Icons.star,
-                    size: 16,
-                    color: Colors.white,
-                  ),
                 ],
               ),
-            ),
-            const SizedBox(height: 8),
-            Text(
-              '\$${song.songETC}',
-              style: const TextStyle(
-                fontSize: 20,
+              const SizedBox(height: 8),
+              Text(
+                song.songJanre,
+                maxLines: 2,
+                style: const TextStyle(fontWeight: FontWeight.w400),
+                overflow: TextOverflow.ellipsis,
               ),
-            ),
-          ],
+              // const SizedBox(height: 8),
+              // Container(
+              //   decoration: BoxDecoration(
+              //     color: Colors.green,
+              //     borderRadius: BorderRadius.circular(12),
+              //   ),
+              //   padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
+              //   child: Row(
+              //     mainAxisSize: MainAxisSize.min,
+              //     children: [
+              //       Text(
+              //         song.id.toString(),
+              //         style: const TextStyle(color: Colors.white),
+              //       ),
+              //       const Icon(
+              //         Icons.star,
+              //         size: 16,
+              //         color: Colors.white,
+              //       ),
+              //     ],
+              //   ),
+              // ),
+              // const SizedBox(height: 8),
+              // Text(
+              //   '\$${song.songETC}',
+              //   style: const TextStyle(
+              //     fontSize: 20,
+              //   ),
+              // ),
+            ],
+          ),
         ),
       ),
     );
